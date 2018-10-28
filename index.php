33216345326580d2c0a67f082a1620e04ae9d34b
Romand1z9 <jamkowoyd1z9@gmail.com>
2018-10-28 10:16:29 +0300

    Изменён title и meta-тег для адаптивности.

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTE
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Start-test</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Start-test!!!</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="/css/bootstrap/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
 </head>
